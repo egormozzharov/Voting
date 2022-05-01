e9a6c15017defd8db186d97982fbf6db70dd3ab2
egormozzharov <egormozzharov@gmail.com>
2022-05-01 18:01:01 +0300

    Rinkeby network was added into hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('solidity-coverage');
+require('dotenv').config();
+const { API_URL, PRIVATE_KEY } = process.env;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,4 +21,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.13",
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
